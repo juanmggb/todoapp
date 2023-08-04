0e4c1216a99462b23382438ba5d6587fdfbd898d
JuanM <58197473+juanmggb@users.noreply.github.com>
2023-08-03 20:33:11 -0600

    JM added todo app

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from todo.models import Task
 
 
 def home(request):
+    pending_tasks = Task.objects.filter(is_completed=False)
 
-    return render(request, "home-todo.html")
+    completed_tasks = Task.objects.filter(is_completed=True)
+
+    context = {
+        "pending_tasks": pending_tasks,
+        "completed_tasks": completed_tasks,
+    }
+
+    return render(request, "home-todo.html", context=context)
